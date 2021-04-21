1209d1110ed14d9067da24c07d7dbd28ef87191e
Giovani Antunes Lakatos <giovaniantunes27@gmail.com>
2021-04-21 18:05:06 -0300

    first

---
+++ src/Components/Header/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 30px;
+  background-color: #7159c1;
+  color: white;
+  display: flex;
+  align-items: center;
+`;
